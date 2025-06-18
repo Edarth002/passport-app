a0ea2935b95c76e3821d7828551a410814382c41
Arthakker <arthuronyeanusi@gmail.com>
2025-06-19 00:10:51 +0100

    Completed image routes

--- routes/image.js
+++ routes/image.js
@@ -1,3 +1,29 @@
 import multer from "multer";
+import express from 'express';
+import path from 'path';
 import { uploadImage } from "../controllers/imageController.js";
 import { myGallery } from "../controllers/imageController.js";
+
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, 'uploads/')
+    },
+    filename:(req, file, cb)=>{
+        const suffix = Date.now
+        const ext = path.extname(file.originalname)
+        cb(null, file.fieldname + "-" + suffix + ext )
+    }
+})
+
+const upload = multer({storage})
+
+//Routes
+
+router.post("/upload", upload.single("image"), uploadImage )
+
+router.post("/gallery", myGallery)
+
+export default router;
